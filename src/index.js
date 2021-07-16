2cf049c21d2ff87be719e53d2d9300f41291aecd
maryam-Si <maryammaryamsadeghi6@gmail.com>
2021-07-16 19:35:49 +0430

    add provider to access state in redux

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import App from "./App";
 import Rtl from "./layout/Rtl";
 import { BrowserRouter as Router } from "react-router-dom";
 import Theme from "./layout/Theme";
+import { Provider } from "react-redux";
+import store from "./store/configureStore";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Rtl>
-        <Theme>
-          <App />
-        </Theme>
-      </Rtl>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Rtl>
+          <Theme>
+            <App />
+          </Theme>
+        </Rtl>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
