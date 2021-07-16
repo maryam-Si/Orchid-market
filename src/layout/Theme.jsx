ace89c1fb71600d87f459a657334a7e9d3d75175
maryam-Si <maryammaryamsadeghi6@gmail.com>
2021-07-16 19:25:48 +0430

    some responsive edits added tp theme

--- src/layout/Theme.jsx
+++ src/layout/Theme.jsx
@@ -1,36 +1,68 @@
-import React from 'react'
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
-import { CssBaseline } from '@material-ui/core';
-import '../assets/fonts/IRYekan/font.css'
+import React from "react";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+import "../assets/fonts/IRYekan/font.css";
 
 export const theme = createTheme({
-    direction: "rtl",
-    typography: {
-      fontFamily: 'IRYekan,sans-serif'
-      
+  direction: "rtl",
+  typography: {
+    fontFamily: "IRYekan,sans-serif",
+  },
+  breakpoints: {
+    values: {
+      xs: 320,
+      sm: 425,
+      md: 768,
+      lg: 1024,
+      xl: 1440,
     },
+  },
+});
+theme.typography.h3 = {
+  [theme.breakpoints.down("xl")]: {
+    fontSize: "2.1rem",
+    fontWeight: "lighter",
+  },
+  [theme.breakpoints.down("lg")]: {
+    fontSize: "2rem",
+    fontWeight: "lighter",
+  },
+  [theme.breakpoints.down("md")]: {
+    fontSize: "1.5rem",
+    fontWeight: "lighter",
+  },
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "1.4rem",
+  },
+  [theme.breakpoints.down("xs")]: {
+    fontSize: "1.2rem",
+    fontWeight: "bold",
+  },
+};
 
-    
-  });
-  theme.typography.h3 = {
-    fontSize: '1.2rem',
-    '@media (min-width:600px)': {
-      fontSize: '0.9rem',
-    },
-    [theme.breakpoints.up('md')]: {
-      fontSize: '1.2rem',
-      fontWeight: 'lighter'
-    },
-  };
+theme.typography.h5 = {
+  [theme.breakpoints.down("xl")]: {},
+  [theme.breakpoints.down("lg")]: {},
+  [theme.breakpoints.down("md")]: {
+    fontSize: "0.7rem",
+    fontWeight: "lighter",
+  },
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "0.9rem",
+  },
+  [theme.breakpoints.down("xs")]: {
+    fontSize: "0.5rem",
+    fontWeight: "bold",
+  },
+};
 
-function Theme({children}) {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            {children}
-        </ThemeProvider>
-    )
+function Theme({ children }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
 }
 
-export default Theme
-
+export default Theme;
