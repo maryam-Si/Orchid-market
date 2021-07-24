e092a82f7bb2508f03a6c54139dfbc2b55e72a35
maryam-Si <maryammaryamsadeghi6@gmail.com>
2021-07-25 01:26:59 +0430

    create basic home page and basketPage

---
+++ src/pages/store/BasketPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function BasketPage() {
+  return (
+    <div style={{ marginTop: "20%" }}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+      exercitationem quia odit. Voluptate asperiores commodi beatae odio
+      excepturi enim est repellendus. Consequuntur reprehenderit omnis ex saepe
+      soluta, in quidem molestias. rLorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Perferendis ducimus dolorem inventore, dolore nisi quod
+      recusandae consectetur minus quam animi, pariatur blanditiis illo quae,
+      unde praesentium porro reprehenderit aspernatur cupiditate? Lorem, ipsum
+      dolor sit amet consectetur adipisicing elit. Dicta vitae explicabo rem,
+      sunt cum perferendis ducimus cumque expedita! Dolore molestias incidunt
+      placeat quod quo animi totam harum consectetur consequuntur praesentium!
+    </div>
+  );
+}
+
+export default BasketPage;
