53b79f5e30fb57aeaa521441563a48d6aaa395d1
maryam-Si <maryammaryamsadeghi6@gmail.com>
2021-07-27 03:39:04 +0430

    add carousel to homePage

---
+++ src/components/store/carousel/Slider.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import autoBind from "auto-bind";
+import "../../../styles/sass/banner.scss";
+import { items } from "./items";
+import Banner from "./Banner";
+
+class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      autoPlay: true,
+      animation: "slide",
+      indicators: true,
+      timeout: 500,
+      navButtonsAlwaysVisible: false,
+      navButtonsAlwaysInvisible: false,
+      cycleNavigation: true,
+    };
+
+    autoBind(this);
+  }
+
+  render() {
+    return (
+      <div style={{ marginTop: "129px", color: "#494949" }}>
+        <Carousel
+          className="Example"
+          autoPlay={this.state.autoPlay}
+          animation={this.state.animation}
+          indicators={this.state.indicators}
+          timeout={this.state.timeout}
+          cycleNavigation={this.state.cycleNavigation}
+          navButtonsAlwaysVisible={this.state.navButtonsAlwaysVisible}
+          navButtonsAlwaysInvisible={this.state.navButtonsAlwaysInvisible}
+        >
+          {items.map((item, index) => {
+            return (
+              <Banner
+                item={item}
+                key={index}
+                contentPosition={item.contentPosition}
+              />
+            );
+          })}
+        </Carousel>
+      </div>
+    );
+  }
+}
+
+export default Slider;
---
+++ src/components/store/carousel/items.js
@@ -0,0 +1,77 @@
+import womenBanner from "../../../assets/images/carousel/woman-black-trousers-purple-blouse-laughs-leaning-stand-with-elegant-clothes-pink-background.jpg";
+import womenBanner2 from "../../../assets/images/carousel/positive-woman-red-beret-trendy-blouse-smiles-holds-bags-from-clothing-stores.jpg";
+import menBanner from "../../../assets/images/carousel/man-with-sunglasses-wearing-white-t-shirt-posing.jpg";
+import menBanner2 from "../../../assets/images/carousel/young-successful-african-american-man-wearing-blue-turtleneck-look-with-satisfied-face-pointing-left.jpg";
+import kids1 from "../../../assets/images/carousel/cute-stylish-children.jpg";
+import kids2 from "../../../assets/images/carousel/group-beautiful-girls-boys-pastel-wall.jpg";
+import beauty1 from "../../../assets/images/carousel/pexels-ron-lach-8131582.jpg";
+import beauty2 from "../../../assets/images/carousel/beauty2.jpg";
+import sport1 from "../../..//assets/images/carousel/pexels-monstera-6311274.jpg";
+import sport2 from "../../../assets/images/carousel/pexels-monstera-6311544.jpg";
+export const items = [
+  {
+    Name: "کالکشن زنانه",
+    Caption: " کیفیت هرگز از مد نمی افتد",
+    contentPosition: "left",
+    Items: [
+      {
+        Image: womenBanner,
+      },
+      {
+        Image: womenBanner2,
+      },
+    ],
+  },
+  {
+    Name: " کالکشن مردانه",
+    Caption: " جایی که مد به کمک زندگی میاد",
+    contentPosition: "middle",
+    Items: [
+      {
+        Image: menBanner,
+      },
+      {
+        Image: menBanner2,
+      },
+    ],
+  },
+  {
+    Name: "کالکشن بچگانه",
+    Caption: " دنیایی از شادی",
+    contentPosition: "right",
+    Items: [
+      {
+        Image: kids1,
+      },
+      {
+        Image: kids2,
+      },
+    ],
+  },
+  {
+    Name: "کالکشن سلامت و زیبایی",
+    Caption: "معجزه جوانی",
+    contentPosition: "right",
+    Items: [
+      {
+        Image: beauty2,
+      },
+      {
+        Image: beauty1,
+      },
+    ],
+  },
+  {
+    Name: "کالکشن ورزشی",
+    Caption: " محنصر به فرد باش! سلامت باش! خوشحال باش !",
+    contentPosition: "middle",
+    Items: [
+      {
+        Image: sport1,
+      },
+      {
+        Image: sport2,
+      },
+    ],
+  },
+];
