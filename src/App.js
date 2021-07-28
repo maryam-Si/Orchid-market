437486c41d7561f602d1ae5a1a956dffde74ea89
maryam-Si <maryammaryamsadeghi6@gmail.com>
2021-07-28 18:16:33 +0430

    add lazy loading to the project

--- src/App.js
+++ src/App.js
@@ -1,58 +1,90 @@
+import React, { Suspense, lazy } from "react";
+import Loading from "./components/loading/Loading";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import LoginAdmin from "./pages/admin/Login/LoginAdmin";
-import AdminPanel from "./pages/admin/admin-panel-page/AdminPanel";
-import PanelProducts from "./pages/admin/panel-products/PanelProducts";
-import PanelOrders from "./pages/admin/panel-orders/PanelOrders";
-import PanelQuantity from "./pages/admin/panel-quantity/PanelQuantity";
 import { ProtectedRoute } from "./ProtectedRoute";
 import NotFound from "./pages/NotFound";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import Container from "./components/store/container/Container";
-import HomePage from "./pages/store/HomePage";
-import BasketPage from "./pages/store/BasketPage";
-
+import { BrowserRouter as Router } from "react-router-dom";
+const HomePage = lazy(() => import("./pages/store/home/HomePage"));
+const BasketPage = lazy(() => import("./pages/store/BasketPage"));
+const CategoryPage = lazy(() => import("./pages/store/category/CategoryPage"));
+const ProductDetail = lazy(() =>
+  import("./pages/store/productDetail/ProductDetail")
+);
+const LoginAdmin = lazy(() => import("./pages/admin/Login/LoginAdmin"));
+const AdminPanel = lazy(() =>
+  import("./pages/admin/admin-panel-page/AdminPanel")
+);
+const PanelProducts = lazy(() =>
+  import("./pages/admin/panel-products/PanelProducts")
+);
+const PanelOrders = lazy(() =>
+  import("./pages/admin/panel-orders/PanelOrders")
+);
+const PanelQuantity = lazy(() =>
+  import("./pages/admin/panel-quantity/PanelQuantity")
+);
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/" exact>
-          <Container>
-            <HomePage />
-          </Container>
-        </Route>
+    <>
+      <div className="App">
+        <Suspense fallback={<Loading />}>
+          <Router>
+            <Switch>
+              <Route path="/" exact>
+                <Container>
+                  <HomePage />
+                </Container>
+              </Route>
 
-        <Route path="/cart" exact>
-          <Container>
-            <BasketPage />
-          </Container>
-        </Route>
+              <Route path="/cart" exact>
+                <Container>
+                  <BasketPage />
+                </Container>
+              </Route>
 
-        <Route path="/admin/login" exact component={LoginAdmin} />
+              <Route path="/category/:categoryName" exact>
+                <Container>
+                  <CategoryPage />
+                </Container>
+              </Route>
+              <Route path="/category/:categoryName/:product/:id" exact>
+                <Container>
+                  <ProductDetail />
+                </Container>
+              </Route>
 
-        <ProtectedRoute path="/admin/panel-products" exact>
-          <AdminPanel>
-            <PanelProducts />
-          </AdminPanel>
-        </ProtectedRoute>
+              <Route path="/admin/login" exact component={LoginAdmin} />
 
-        <ProtectedRoute path="/admin/panel-quantity" exact>
-          <AdminPanel>
-            <PanelQuantity />
-          </AdminPanel>
-        </ProtectedRoute>
+              <ProtectedRoute path="/admin/panel-products" exact>
+                <AdminPanel>
+                  <PanelProducts />
+                </AdminPanel>
+              </ProtectedRoute>
 
-        <ProtectedRoute path="/admin/panel-orders" exact>
-          <AdminPanel>
-            <PanelOrders />
-          </AdminPanel>
-        </ProtectedRoute>
+              <ProtectedRoute path="/admin/panel-quantity" exact>
+                <AdminPanel>
+                  <PanelQuantity />
+                </AdminPanel>
+              </ProtectedRoute>
 
-        <Route path="*" component={NotFound} />
-      </Switch>
-      <ToastContainer />
-    </div>
+              <ProtectedRoute path="/admin/panel-orders" exact>
+                <AdminPanel>
+                  <PanelOrders />
+                </AdminPanel>
+              </ProtectedRoute>
+
+              <Route path="*" component={NotFound} />
+            </Switch>
+          </Router>
+        </Suspense>
+
+        <ToastContainer />
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Rtl from "./layout/Rtl";
-import { BrowserRouter as Router } from "react-router-dom";
 import Theme from "./layout/Theme";
 import { Provider } from "react-redux";
 import store from "./store/configureStore";
@@ -10,13 +9,11 @@ import store from "./store/configureStore";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Rtl>
-          <Theme>
-            <App />
-          </Theme>
-        </Rtl>
-      </Router>
+      <Rtl>
+        <Theme>
+          <App />
+        </Theme>
+      </Rtl>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
